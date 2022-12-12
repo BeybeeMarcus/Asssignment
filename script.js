008201382101a75ac63d241cf08c41e2ebf6f2d9
Mark Lawrence Peralta <vfranco123456@gmail.com>
2022-12-12 15:03:21 +0800

    1

---
+++ script.js
@@ -0,0 +1,8 @@
+const prompt1 = "Enter first number"
+const prompt2 = "Enter second number"
+console.log(`Addition: ${num1 + num2}`);
+console.log(`Subtraction: ${num1 + num2}`);
+console.log(`Multiplication: ${num1 + num2}`);
+console.log(`Division: ${num1 + num2}`);
+console.log(`Modulo: ${num1 + num2}`);
+
